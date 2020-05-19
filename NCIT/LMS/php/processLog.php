d158482613d1b07143f4ead360ac6bc02eb68798
DroidDileep <58536954+DroidDileep@users.noreply.github.com>
2020-05-19 11:57:01 +0545

    changed table.php and fillLog.php and also process

--- NCIT/LMS/php/processLog.php
+++ NCIT/LMS/php/processLog.php
@@ -8,22 +8,24 @@
 		$tid=$_POST['tid'];
 		$sub=$_POST['subject'];
 		$topics=$_POST['topics'];
+		$ctype=$_POST['ctype'];
 		$time=$_POST['time'];
 		$nop=$_POST['nop'];
 		$nos=$_POST['nos'];
+		$remarks=$_POST['remarks'];
 
 		#connection to localhost and db selection
 		$conn=mysqli_connect("localhost","root","") or die("connection error");
 
 		$db=mysqli_select_db($conn,"lms") or die("db error");
 
-		$query="INSERT INTO logsheet(id,date,teacher_id,subject,topics,time,nop,nos) VALUES('','$date','$tid','$sub','$topics','$time','$nop','$nos')";
+		$query="INSERT INTO logsheet(id,date,teacher_id,subject,topics,class_type,time,nop,nos,remarks) VALUES('','$date','$tid','$sub','$topics','$ctype','$time','$nop','$nos','$remarks')";
 
 		if(mysqli_query($conn,$query)){
 			echo "log sheet updated!";
 		}
 		else{
-			echo "error in updating log sheet!";
+			echo "error in updating log sheet!".mysqli_error($conn);
 		}
 
 	}
