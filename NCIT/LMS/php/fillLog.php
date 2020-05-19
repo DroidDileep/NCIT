c8e0105374eebb8abb5b337c0f751d198a0511f4
DroidDileep <58536954+DroidDileep@users.noreply.github.com>
2020-05-19 11:46:20 +0545

    changed table.php and fillLog.php

--- NCIT/LMS/php/fillLog.php
+++ NCIT/LMS/php/fillLog.php
@@ -5,7 +5,7 @@
 	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 	<style type="text/css">
 		.main{
-			width:80%;
+			width:90%;
 			margin: auto;
 			margin-top: 100px;
 		}
@@ -14,7 +14,6 @@
 <body>
 
 	<div class="container-fluid">
-
 		<div class="main justify-content-center">
 			<h3 class="text-success">Provide the class details</h3>
 			<form class="form-inline" action="processLog.php" method="POST">
@@ -31,13 +30,29 @@
 					<input type="text" class="form-control form-control-sm" name="topics" placeholder="Topics" required>
 				</div>
 				<div class="form-group mr-2 col-xs-2">
-					<input type="text" class="form-control form-control-sm" name="time" placeholder="Time" required>
+					<div class="form-check-inline">
+  						<label class="form-check-label">
+    						<input type="radio" class="form-check-input" name="ctype" value="L" checked="checked">Lect
+  						</label>
+					</div>
+					<div class="form-check-inline">
+  						<label class="form-check-label">
+    						<input type="radio" class="form-check-input" name="ctype" value="P">Pract
+  						</label>
+					</div>
+				</div>
+
+				<div class="form-group mr-2 col-xs-2">
+					<input type="text" class="form-control form-control-sm" name="time" placeholder="Time" style="width: 9em"required>
 				</div>
 				<div class="form-group mr-2 col-xs-2">
 					<input type="number" class="form-control form-control-sm" style="width: 7em" name="nop" placeholder="No.of Periods" required>
 				</div>
 				<div class="form-group mr-2 col-xs-2">
 					<input type="number" class="form-control form-control-sm" style="width: 7em" name="nos" placeholder="No.of Students">
+				</div>
+				<div class="form-group mr-2 col-xs-2">
+					<input type="text" class="form-control form-control-sm" name="remarks" placeholder="Remarks Any">
 				</div>
 				<div class="form-group mr-2 col-xs-2 mt-2">
 					<input type="submit" class="form-control form-control-sm btn-primary" name="filllog" value="Submit">
